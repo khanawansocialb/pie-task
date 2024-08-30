afa176e8304321ebc698681f6efdde93e985a9b4
khanawan62 <56925336+khanawan62@users.noreply.github.com>
2024-08-31 00:24:09 +0500

    added user modules and converted root widget to stateful widget to listen to app lifecycle

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:pie_task/config/app_size.dart';
 import 'package:pie_task/config/routes.dart';
 import 'package:pie_task/home/cubit/products_cubit.dart';
 import 'package:pie_task/home/service/product_service.dart';
+import 'package:pie_task/user/service/user_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,8 +17,40 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with WidgetsBindingObserver{
+  final UserService _userService = UserService();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+
+    if (state == AppLifecycleState.resumed) {
+      _userService.setUserStatus(UserStatus.online);
+    } else if (state == AppLifecycleState.paused || state == AppLifecycleState.inactive) {
+      _userService.setUserStatus(UserStatus.away);
+    } else if (state == AppLifecycleState.detached) {
+      _userService.setUserStatus(UserStatus.offline);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
---
+++ lib/user/service/user_service.dart
@@ -0,0 +1,14 @@
+enum UserStatus {
+  online,
+  away,
+  offline
+}
+
+class UserService {
+  ///in a real app the status of the user would be
+  ///commuicated to the backend 
+  
+  setUserStatus (UserStatus userStatus) {
+    
+  }
+}
